16a79c2f2794d796cd09bc57893ecc704a73ddac
altoliaw <altoliaw@hotmail.com>
2014-11-21 02:19:04 +0800

    Kill Say Story

--- sayStory/ajax_getGUID.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$ArrayText_GUID['GUID'] = "588b14f5-f9f5-4471-a4e7-3d70ff667d6a";
-	echo json_encode($ArrayText_GUID);
-?>
--- sayStory/ajax_setGameStart.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$Int_Status['success'] = 1;
-	echo json_encode($Int_Status);
-?>
